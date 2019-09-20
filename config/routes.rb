92e7a03bd2c1e8251cd23af835eae0aab38fe74f
tjharo <orvis952002@yahoo.com>
2019-09-20 03:43:59 +0000

    Halfway through about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'quotes#index'
   resources :quotes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'quotes#about'
 end
